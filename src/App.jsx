e144ce4a5ac14a9215245e6a90da77ecd0b311ca
Liubov Peshkova <maximliuba2@gmail.com>
2024-09-11 10:53:07 +0200

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Lesson02 from './lessons/lesson02/Lesson02';
+
 
 function App() {
   return (
     <div className="App">
-    My React project
+ 
+    <Lesson02/>
+    
     </div>
   );
 }
---
+++ src/lessons/lesson02/Lesson02.jsx
@@ -0,0 +1,9 @@
+import Button from "../../components/Button/Button"
+import "./styles.css"
+ export default function Lesson02() {
+  return (<div className='buttons-container'>
+    <Button />
+    <Button />
+    <Button />
+ </div>)
+ }
