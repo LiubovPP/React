ee0a4ceabdbf832042712fe611d8b0998d0bbd46
Liubov Peshkova <maximliuba2@gmail.com>
2024-09-11 12:49:09 +0200

    add ProfileCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
+import Homework02 from './homework/homework02/Homework02';
 import Lesson02 from './lessons/lesson02/Lesson02';
 
 
 function App() {
   return (
     <div className="App">
- 
-    <Lesson02/>
-    
+    {/* <Lesson02 /> */}
+    <Homework02 />
     </div>
   );
 }
---
+++ src/homework/homework02/Homework02.jsx
@@ -0,0 +1,15 @@
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import "./styles.css";
+
+function Homework02 () {
+  return (<div className="profile-cards-container">
+    <div className="profile-card-control"><ProfileCard /></div>
+    
+    
+    <ProfileCard />
+    <ProfileCard />
+  </div>)
+
+}
+
+export default Homework02;
